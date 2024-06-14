21dc49e0b3f0d041718de49152bb69311c260779
Eetu Reijonen <eetujj.reijonen@gmail.com>
2024-06-14 15:48:55 +0300

    added oil case study files

---
+++ model_and_data_viz.jl
@@ -0,0 +1,32 @@
+using CSV
+using DataFrames
+using Plots
+using JuMP
+using GLPK # because my Gurobi license is expired
+using JSON
+
+include("ICNN_to_LP.jl")
+
+for w in 1:8
+    oil_model = Model(GLPK.Optimizer)
+    @objective(oil_model, Max, 0)
+
+    @variable(oil_model, p)
+    @variable(oil_model, out)
+
+    ICNN_formulate!(oil_model, "ICNN_well_$w.json", out, p)
+
+    df = CSV.read("well_$w.csv", DataFrame)
+
+    x_range = LinRange{Float32}(minimum(df.PWH), maximum(df.PWH), 50)
+
+    plot(x_range, x -> -forward_pass_ICNN!(oil_model, x, out, p), label="ICNN", title="WELL $w", xlabel="pressure", ylabel="flow")
+
+    display(scatter!(df.PWH, df.QOIL, label="Data"))
+end
+
+df = CSV.read("flowline_1.csv", DataFrame)
+scatter(df.QOIL, df.PDS, markersize=1, label="data", title="FLOWLINE PRESSURE", xlabel="oil flow")
+scatter(df.QGAS, df.PDS, markersize=1, label="data", title="FLOWLINE PRESSURE", xlabel="gas flow")
+scatter(df.QWAT, df.PDS, markersize=1, label="data", title="FLOWLINE PRESSURE", xlabel="water flow")
+scatter(df.PUS, df.PDS, markersize=1, label="data", title="FLOWLINE PRESSURE", xlabel="upstream pressure")
---
+++ oil_milp.jl
@@ -0,0 +1,134 @@
+# Oil optimization problem as described in Grimstad and Anderson (2019)
+
+### GRAPH STRUCTURE ###
+
+Nw = [i for i in 1:8]
+Nm = [9, 10]
+Ns = [11, 12]
+N = union(Nw, Nm, Ns)
+
+Ed = vec([(i, j) for i in 1:8, j in [9, 10]])
+Er = [(9, 11), (10, 12)]
+E = union(Ed, Er)
+
+Ein = Vector{Vector}(undef, 12)
+[Ein[i] = [] for i in 1:8]
+Ein[9] = [(i, 9) for i in 1:8]
+Ein[10] = [(i, 10) for i in 1:8]
+Ein[11] = [(9, 11)]
+Ein[12] = [(10, 12)]
+
+Eout = Vector{Vector}(undef, 12)
+[Eout[i] = [(i, 9), (i, 10)] for i in 1:8]
+Eout[9] = [(9, 11)]
+Eout[10] = [(10, 12)]
+Eout[11] = []
+Eout[12] = []
+
+C = [1, 2, 3] # oil, gas, water
+
+### CONSTANTS ###
+
+P_LIMS = [ # pressure limits for each node
+    (0.3, 1.65329),
+    (0.3, 1.55873),
+    (0.3, 1.76193),
+    (0.3, 1.51174),
+    (0.3, 1.44545),
+    (0.3, 1.32290),
+    (0.3, 1.62300),
+    (0.3, 1.24020),
+    (0.299867, 2.09987),
+    (0.299867, 2.09987),
+    (0.005022, 1.33179),
+    (0.005022, 1.33179),
+]
+
+Q_OIL = [ # oil flow bounds from the wells
+    (0.0, 0.240734),
+    (0.0, 0.284504),
+    (0.0, 0.190091),
+    (0.0, 0.162084),
+    (0.0, 0.090637),
+    (0.0, 0.232649),
+    (0.0, 0.269781),
+    (0.0, 0.323648),
+
+]
+
+Q_GAS = (0.024000, 1.92000) # gas flow bounds from the wells ??
+
+Q_WATER = (0.000000, 0.96000) # water flow bounds from the wells ??
+
+GOR = [1053.0, 805.0, 789.0, 790.0, 633.0, 745.0, 742.0, 759.0] .* 0.001 # gas-oil ratio
+
+WOR = [12.7, 23.7, 41.7, 55.4, 54.81, 27.0, 0, 0] # water-oil ratio
+
+P_SEP = [0.20, 0.30] # separator pressures
+
+### JUMP MODEL ###
+
+using JuMP
+using GLPK # because my Gurobi license is expired
+
+include("ICNN_to_LP.jl")
+
+oil_model = Model(GLPK.Optimizer)
+
+@variable(oil_model, p[N])
+@variable(oil_model, q[E, C])
+@variable(oil_model, y[Ed], Bin) # 14l
+
+# 14a
+@objective(oil_model, Max, sum([q[e, 1] for e in Er]))
+
+# 14b
+@constraint(oil_model, [c=C, i=Nm], sum([q[e, c] for e in Ein[i]]) == sum([q[e, c] for e in Eout[i]]))
+
+# 14c
+@variable(oil_model, g[e=Er])
+[ICNN_formulate!(oil_model, "ICNN_flowline_1.json", g[e], q[e, 1], q[e, 2], q[e, 3], p[e[1]]) for e in Er]
+@constraint(oil_model, [e in Er], p[e[2]] == g[e])
+
+# 14d
+@constraint(oil_model, [e=Ed], p[e[1]] - p[e[2]] <= (P_LIMS[e[1]][2] - P_LIMS[e[2]][1]) * (1 - y[e]))
+@constraint(oil_model, [e=Ed], (-P_LIMS[e[2]][2] + P_LIMS[e[1]][1]) * (1 - y[e]) <= p[e[1]] - p[e[2]])
+
+# 14e
+@constraint(oil_model, [i=Nw], sum([y[e] for e in Eout[i]]) <= 1)
+
+# 14f
+@constraint(oil_model, [e=Ed], y[e] * Q_OIL[e[1]][1] <= q[e, 1])
+@constraint(oil_model, [e=Ed], q[e, 1] <= y[e] * Q_OIL[e[1]][2])
+
+# gas
+@constraint(oil_model, [e=Ed], y[e] * Q_GAS[1] <= q[e, 2])
+@constraint(oil_model, [e=Ed], q[e, 2] <= y[e] * Q_GAS[2])
+
+# water
+@constraint(oil_model, [e=Ed], y[e] * Q_WATER[1] <= q[e, 3])
+@constraint(oil_model, [e=Ed], q[e, 3] <= y[e] * Q_WATER[2])
+
+# 14g pressure limits at each node
+@constraint(oil_model, [i=N], P_LIMS[i][1] <= p[i] <= P_LIMS[i][2])
+
+# 14h
+@variable(oil_model, f[i=Nw])
+[ICNN_formulate!(oil_model, "ICNN_well_$i.json", f[i], p[i]) for i in Nw]
+@constraint(oil_model, [i=Nw], sum([q[e, 1] for e in Eout[i]]) == -f[i])
+
+# 14i
+@constraint(oil_model, [i=Nw], sum([q[e, 2] for e in Eout[i]]) == GOR[i] * sum([q[e, 1] for e in Eout[i]]))
+
+# 14j
+@constraint(oil_model, [i=Nw], sum([q[e, 3] for e in Eout[i]]) == WOR[i] * sum([q[e, 1] for e in Eout[i]]))
+
+# 14k
+@constraint(oil_model, [i=Ns], p[i] == P_SEP[i-10])
+
+### SOLUTION ###
+
+objective_function(oil_model)
+optimize!(oil_model)
+solution_summary(oil_model)
+objective_value(oil_model)
